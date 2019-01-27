e1f711d711383096e6723aa001016c30476ce31d
Andy Su <f2280c@gmail.com>
2019-01-27 03:08:36 -0500

    google cloud api can't support decodeBase64 method

--- app/src/main/java/example/com/weisu/MainActivity.java
+++ app/src/main/java/example/com/weisu/MainActivity.java
@@ -3,11 +3,18 @@ package example.com.weisu;
 import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import com.google.api.Service;
+import com.google.api.client.util.IOUtils;
 import com.google.auth.Credentials;
 import com.google.auth.oauth2.ServiceAccountCredentials;
 
-import java.io.File;
-import java.io.FileInputStream;
+import java.io.*;
+import java.net.URI;
+import java.nio.Buffer;
+
+import com.google.auth.oauth2.GoogleCredentials;
+
+//import com.google.api.client.googleapis.auth.oauth2.GoogleCredential;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -16,16 +23,31 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        Credentials myCredentials = null;
 
-        try{
-            String path = "android.resource://" + getBaseContext().getPackageName() +  "/" + R.raw.weisuserviceaccount;
-            Uri uri = Uri.parse(path);
-//            System.out.println("uri: " + uri.toString());
-            File file = new File(uri.getPath());
-            myCredentials = ServiceAccountCredentials.fromStream(
-                    new FileInputStream(file));
-        } catch(Exception e){
+        try {
+            InputStream inputStream = this.getResources().openRawResource(R.raw.weisu);
+
+            String fileName = "cred.json";
+
+            FileOutputStream outputStream;
+            outputStream = openFileOutput(fileName, this.MODE_PRIVATE);
+            IOUtils.copy(inputStream, outputStream);
+            outputStream.close();
+
+            String fileLocation = MainActivity.this.getFilesDir().getAbsolutePath();
+
+            String fileAbsoluteLocation = fileLocation + "/" + fileName;
+
+            BufferedReader bf = new BufferedReader(new FileReader(new File(fileAbsoluteLocation)));
+
+            System.out.println(bf.readLine());
+
+            System.out.println(new File(fileAbsoluteLocation));
+
+            Credentials myCredentials = ServiceAccountCredentials.fromStream(
+                    new FileInputStream(new File(fileLocation + "/" + fileName)));
+
+        } catch (Exception e) {
             e.printStackTrace();
         }
 
